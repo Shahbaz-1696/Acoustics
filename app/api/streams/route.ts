249b6cfb0991ba43cca00d7d222d2b1848c755ff
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2024-09-02 22:57:42 +0530

    Added upvotes, downvotes andstream backend endpoints

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -1,5 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
+import { prismaClient } from "@/app/lib/db";
+
+const YT_REGEX = new RegExp("^https://www.youtube.com/watch?v=[w-]{11}$");
 
 const CreateStreamSchema = z.object({
   creatorId: z.string(),
@@ -9,6 +12,30 @@ const CreateStreamSchema = z.object({
 export async function POST(req: NextRequest) {
   try {
     const data = CreateStreamSchema.parse(await req.json());
+    const isYoutube = YT_REGEX.test(data.url);
+
+    if (!isYoutube) {
+      return NextResponse.json(
+        {
+          message: "Wrong URL format",
+        },
+        {
+          status: 411,
+        }
+      );
+    }
+
+    const extractedId = data.url.split("?v=")[1];
+
+    await prismaClient.stream.create({
+      data: {
+        userId: data.creatorId,
+        url: data.url,
+        extractedId,
+        type: "Youtube",
+      },
+    });
+    
   } catch (error) {
     return NextResponse.json(
       {
