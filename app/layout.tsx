19df7abb3e791d2aad7732db11dd37bfae151731
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2024-09-11 21:47:47 +0530

    Updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Acoustics",
   description: "Give a push to your music taste",
+  icons: {
+    icon: "/public/music.svg",
+  },
 };
 
 export default function RootLayout({
