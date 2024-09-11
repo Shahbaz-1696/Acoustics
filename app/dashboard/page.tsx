ff08f20d0cbdce9a0f95498fbd7ba44cb4de726f
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2024-09-11 19:21:06 +0530

    Updated creatorId in dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,11 @@
 import { getServerSession } from "next-auth";
 import StreamView from "../components/StreamView";
 import { authOptions } from "@/lib/authOptions";
-import { redirect } from "next/dist/server/api-utils";
+import { redirect } from "next/navigation";
 
-const creatorId = "91cf1cf2-9da3-4f28-b9c5-cb22ccd88aaf";
 
 export default async function Component() {
   const session = await getServerSession(authOptions);
-  
-  return <StreamView creatorId={creatorId} playVideo={true} />;
+  if (!session?.user.id) redirect("/");
+  return <StreamView creatorId={session?.user.id} playVideo={true} />;
 }
--- hooks/useRedirect.ts
+++ hooks/useRedirect.ts
@@ -1,17 +1,17 @@
 import { useEffect } from "react";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-
+import { usePathname, useRouter } from "next/navigation";
 
 export default function useRedirect() {
-    const session = useSession();
-    const router = useRouter();
+  const session = useSession();
+  const router = useRouter();
+  const pathname = usePathname();
 
-    useEffect(() => {
-        if (session.status === "authenticated") {
-            router.push("/dashboard")
-        } else {
-            router.push("/")
-        }
-    }, [session])
+  useEffect(() => {
+    if (session.status === "authenticated") {
+      router.push("/dashboard");
+    } else if (!pathname.includes("creator")) {
+      router.push("/");
+    }
+  }, [session]);
 }
