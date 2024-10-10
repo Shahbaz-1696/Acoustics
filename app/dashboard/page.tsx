9db9f6744ee431157be699df56b9487a40f6a463
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2024-10-10 18:42:43 +0530

    Bug fixes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,9 +3,8 @@ import StreamView from "../components/StreamView";
 import { authOptions } from "@/lib/authOptions";
 import { redirect } from "next/navigation";
 
-
 export default async function Component() {
   const session = await getServerSession(authOptions);
-  if (!session?.user.id) redirect("/");
-  return <StreamView creatorId={session?.user.id} playVideo={true} />;
+  // if (!session?.user?.id) redirect("/");
+  return <StreamView creatorId={session?.user?.id} playVideo={true} />;
 }
